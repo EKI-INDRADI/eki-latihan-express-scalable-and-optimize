ee9c4f006311785ff66cd2386eebe449261ffa14
EKI-INDRADI <eki.indradi@gmail.com>
2021-08-14 14:31:30 +0700

    update 14-08-2021

---
+++ 5 Section 5 Redis Cache/21 Example - Redis Publisher and Subscirber/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const redis = require("redis");
+
+const keys = require("./keys");
+
+const app = express();
+
+let client = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort
+});
+
+app.get("/", (request, response) => {
+    console.log(`Process ID is ${process.pid}`);
+    let num = request.query.number;
+    if (num % 2 === 0) {
+        client.publish("math-subscription1", num);
+    }
+    else {
+        client.publish("math-subscription2", num);
+    }
+    response.end("<h3>Notification sent to the respective subscribers! We will send you an email with the details!</h3>");
+});
+
+app.listen(3000, () => console.log("Express App is running on PORT : 3000"));
